632bf9641ad79e68533f826fe11316ec579e1fc3
alfaniferdiansah <ferdiansah1808@gmail.com>
2023-08-08 17:27:43 +0700

    update loader

--- backend-studycase/database/index.js
+++ backend-studycase/database/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 const { dbUser, dbPass, dbHost, dbPort, dbName } = require('../config/index')
 
-// mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`)
+mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`)
 
-mongoose.connect(`mongodb+srv://admin:xdwEEMTj2QxIwg8h@cluster0.ntdmhwi.mongodb.net/studycase?retryWrites=true&w=majorityx`)
+// mongoose.connect(`mongodb+srv://admin:xdwEEMTj2QxIwg8h@cluster0.ntdmhwi.mongodb.net/studycase?retryWrites=true&w=majorityx`)
 
 const db = mongoose.connection
 
--- studycase-frontend/src/components/Layout/Loader.jsx
+++ studycase-frontend/src/components/Layout/Loader.jsx
@@ -4,11 +4,9 @@ import "./Loader.css";
 const Loader = () => {
   return(
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="lds-ripple">
-        <div></div>
-        <div></div>
-      </div>
-      <div>Loading.....</div>
+      <div class="loadingio-spinner-spin-8vu6nbgn38b"><div class="ldio-mntd4n1ym5g">
+<div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+</div></div>
     </div>
   )
 };
